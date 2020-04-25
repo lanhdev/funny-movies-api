b02b39b25b700737942a9426dd16ada637ccfa68
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-25 23:01:41 +0700

    Add signin, signout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, only: []
+
+  devise_scope :users do
+    scope :auth, defaults: { format: :json } do
+      post   '/signin',       to: 'sessions#create'
+      delete '/signout',      to: 'sessions#destroy'
+    end
+  end
 end
