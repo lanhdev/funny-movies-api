28ed6dc6bd8ae767d94d0f6cd00a035c0b890b1d
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-25 22:19:29 +0700

    Add User spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { user }
+
+  let(:user) { build(:user) }
+
+  it { is_expected.to respond_to(:name) }
+  it { is_expected.to respond_to(:email) }
+  it { is_expected.to respond_to(:password) }
+  it { is_expected.to respond_to(:password_confirmation) }
+
+  it { is_expected.to be_valid }
+
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email) }
+end
