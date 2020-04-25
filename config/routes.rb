c48918230093b857aa3c00b96e1211cd01cc0cfc
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-26 00:55:53 +0700

    Modify sign_in, sign_out route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users, only: []
-
-  devise_scope :users do
-    scope :auth, defaults: { format: :json } do
-      post   '/signin',       to: 'sessions#create'
-      delete '/signout',      to: 'sessions#destroy'
-    end
-  end
+  devise_for :users, path: 'auth', controllers: { sessions: 'sessions' }
 end
