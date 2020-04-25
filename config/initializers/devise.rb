35a3abb01492a89752ae2050a7591640477c386c
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-26 00:56:13 +0700

    Modify configuration Devise-JWT

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -301,10 +301,10 @@ Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
     jwt.dispatch_requests = [
-      ['POST', %r{^/signin$}]
+      ['POST', %r{^auth/sign_in$}]
     ]
     jwt.revocation_requests = [
-      ['DELETE', %r{^/signout$}]
+      ['DELETE', %r{^auth/sign_out$}]
     ]
     jwt.expiration_time = 1.day.to_i
   end
