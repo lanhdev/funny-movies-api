ecae198d683cc5ce34e93826c026d6a2fa10fd45
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-26 11:16:27 +0700

    Add Request::AuthenticateHelper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,7 +32,8 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include Request::JsonHelpers
+  config.include Request::AuthenticateHelper, type: :request
+  config.include Request::JsonHelper
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -1,7 +1,30 @@
 module Request
-  module JsonHelpers
+  module JsonHelper
     def json_response
       @json_response ||= JSON.parse(response.body, symbolize_names: true)
+    end
+  end
+
+  module AuthenticateHelper
+    include Warden::Test::Helpers
+
+    def self.included(base)
+      base.before(:each) { Warden.test_mode! }
+      base.after(:each) { Warden.test_reset! }
+    end
+
+    def sign_in(resource)
+      login_as(resource, scope: warden_scope(resource))
+    end
+
+    def sign_out(resource)
+      logout(warden_scope(resource))
+    end
+
+    private
+
+    def warden_scope(resource)
+      resource.class.name.underscore.to_sym
     end
   end
 end
