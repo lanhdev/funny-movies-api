f2c18ff03bbfdfccc38c9918ae40e384fd526525
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-26 12:50:37 +0700

    Update migration of User table

--- db/migrate/20200425152323_create_jwt_blacklists.rb
+++ db/migrate/20200425152323_create_jwt_blacklists.rb
@@ -3,8 +3,6 @@ class CreateJwtBlacklists < ActiveRecord::Migration[5.2]
     create_table :jwt_blacklist do |t|
       t.string :jti, null: false
       t.datetime :exp, null: false
-
-      t.timestamps
     end
 
     add_index :jwt_blacklist, :jti
