b89a90c0567e0e9de7fd4fab24d9e9b6c33d94bc
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-26 08:30:29 +0700

    Sign in user after signing up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,7 @@ class SessionsController < Devise::SessionsController
   def sign_up_user
     user = User.new(user_params)
     if user.save
+      sign_in user, store: false
       render json: user, status: 201
     else
       render json: { errors: user.errors }, status: 422
