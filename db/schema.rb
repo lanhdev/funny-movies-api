890f28c965d9e0e55a988da52695f150a735923b
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-27 20:58:28 +0700

    Modify migration

--- db/schema.rb
+++ db/schema.rb
@@ -31,14 +31,13 @@ ActiveRecord::Schema.define(version: 2020_04_26_013502) do
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
-    t.string "email", default: "", null: false
+    t.string "email"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
