69a1c3fc11b26b599534c634af8193beca835888
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-05-02 23:33:18 +0700

    Add ratings attribute to MovieSerializer

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,3 +1,15 @@
+# frozen_string_literal: true
+
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :url, :username
+  attributes :id, :url, :username, :likes_count, :dislikes_count
+
+  has_many :ratings
+
+  def likes_count
+    object.likes.size
+  end
+
+  def dislikes_count
+    object.dislikes.size
+  end
 end
